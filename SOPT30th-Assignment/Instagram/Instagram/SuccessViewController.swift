8d62aba432cc885b006c418c4c1241d73f7bf808
SojinLee <513sojin@naver.com>
2022-04-06 23:55:45 +0900

    [FEAT] 로그인 완료 화면 UI 구성

--- SOPT30th-Assignment/Instagram/Instagram/SuccessViewController.swift
+++ SOPT30th-Assignment/Instagram/Instagram/SuccessViewController.swift
@@ -9,22 +9,26 @@ import UIKit
 
 class SuccessViewController: UIViewController {
 
+  
+    @IBOutlet weak var welcomeInsta: UILabel!
     @IBOutlet weak var welcomeText: UILabel!
+    
+    @IBOutlet weak var doneSignupButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        welcomeInsta.text="OOO님 Instagram에 \n 오신 것을 환영합니다"
+        welcomeInsta.numberOfLines = 5
+        welcomeInsta.font = UIFont.systemFont(ofSize:24)
+        
+        //TODO : UI 스타일링을 여기서 줘야만 하는지, 재사용 가능한 상태로는 못만드는지 공부해보기
+        welcomeText.text = "언제든지 연락처 정보와 사용자 이름을 변경할 수 있습니다."
+        welcomeText.font = UIFont.systemFont(ofSize:14)
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func goToLogin(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
     }
-    */
 
 }
