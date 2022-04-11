bc7e8c0503dbd67ba7b45c56cbe076e742677573
SojinLee <513sojin@naver.com>
2022-04-12 00:24:41 +0900

    [CHORE] 주석 추가 및 userName설정 함수화

--- SOPT30th-Assignment/Instagram/Instagram/SuccessViewController.swift
+++ SOPT30th-Assignment/Instagram/Instagram/SuccessViewController.swift
@@ -8,25 +8,31 @@
 import UIKit
 
 class SuccessViewController: UIViewController{
-    
+    // MARK: - @IBOutlet
     @IBOutlet weak var welcomeText: UILabel!
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var doneSignupButton: UIButton!
     var userName: String?
     
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        configUI()
+        configureUI()
+        setUserNameLabel()
+    }
+    
+    // MARK: - UI
+    private func configureUI(){
+        welcomeText.text = "언제든지 연락처 정보와 사용자 이름을 변경할 수 있습니다."
+    }
+    
+    private func setUserNameLabel(){
         if let userName = userName {
             userNameLabel.text = "\(userName)님 Instagram에 \n 오신 것을 환영합니다."
         }
     }
     
-    func configUI(){
-        welcomeText.text = "언제든지 연락처 정보와 사용자 이름을 변경할 수 있습니다."
-    }
-    
-
+    // MARK: - @IBAction
     @IBAction func goToLogin(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
