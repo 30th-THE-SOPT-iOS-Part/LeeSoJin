e05e46727bbd1e69583464ed01e3c1376c264978
SojinLee <513sojin@naver.com>
2022-04-12 00:21:42 +0900

    [FEAT] viewWillAppear 생명주기 추가

--- SOPT30th-Assignment/Instagram/Instagram/SignupSecondViewController.swift
+++ SOPT30th-Assignment/Instagram/Instagram/SignupSecondViewController.swift
@@ -8,31 +8,48 @@
 import UIKit
 
 class SignupSecondViewController: UIViewController {
+    // MARK: - @IBOutlet
     @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var textLabel: UILabel!
     @IBOutlet weak var noticeLabel: UILabel!
     @IBOutlet weak var passwordTextField: UITextField!
     var userName : String?
     
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-       configUI()
-        self.passwordTextField.addTarget(self, action: #selector(self.passwordTextFieldDidChange(_:)), for: .editingChanged)
+        configureUI()
+        setUpTextField()
     }
     
-    func configUI(){
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        resetTextField()
+    }
+    
+    // MARK: - UI
+    private func configureUI(){
         noticeLabel.text = "비밀번호를 저장할 수 있으므로 iCloud 기기에서 로그인 \n정보를 입력하지 않아도 됩니다."
         nextButton.isEnabled = false
     }
     
+    // MARK: - Custom Methods
+    private func setUpTextField(){
+        passwordTextField.addTarget(self, action: #selector(self.passwordTextFieldDidChange(_:)), for: .editingChanged)
+    }
+    
+    private func resetTextField(){
+        passwordTextField.text?.removeAll()
+    }
+    
+    // MARK: - @IBAction
     @IBAction func nextButtonTap(_ sender: Any) {
         guard let nextViewController = self.storyboard?.instantiateViewController(withIdentifier: "SuccessViewController") as? SuccessViewController else { return }
-        
         nextViewController.userName = userName
         self.present(nextViewController, animated: true, completion: nil)
     }
 
     @objc func passwordTextFieldDidChange(_ sender: Any?) {
-        nextButton.isEnabled = true
+        nextButton.isEnabled = passwordTextField.hasText ? true : false
     }
 }
