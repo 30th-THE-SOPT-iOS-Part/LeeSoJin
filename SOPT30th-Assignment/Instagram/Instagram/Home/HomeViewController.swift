437f1f03c2f50a2a836aafe336ee6f034344b957
SojinLee <513sojin@naver.com>
2022-05-08 23:12:52 +0900

    [FEAT] homeVC에 두번째 section 추가

--- SOPT30th-Assignment/Instagram/Instagram/Home/HomeViewController.swift
+++ SOPT30th-Assignment/Instagram/Instagram/Home/HomeViewController.swift
@@ -8,80 +8,62 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
     // MARK: - @IBOutlet
-    @IBOutlet weak var storyCollectionView: UICollectionView!
     @IBOutlet weak var feedTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        registerXib()
+        registerDelegate()
+    }
 
-        let nib = UINib(nibName: StoryCollectionViewCell.identifier, bundle: nil)
-        storyCollectionView.register(nib, forCellWithReuseIdentifier: StoryCollectionViewCell.identifier)
-        
-        let feedNib = UINib(nibName: FeedTableViewCell.identifier, bundle: nil)
-        feedTableView.register(feedNib, forCellReuseIdentifier: FeedTableViewCell.identifier)
-        
-        storyCollectionView.delegate = self
-        storyCollectionView.dataSource = self
-        
+    private func registerXib(){
+        let feedCell = UINib(nibName: FeedTableViewCell.identifier, bundle: nil)
+        feedTableView.register(feedCell, forCellReuseIdentifier: FeedTableViewCell.identifier)
+        let storyCell = UINib(nibName: StoryTableViewCell.identifier, bundle: nil)
+        feedTableView.register(storyCell, forCellReuseIdentifier: StoryTableViewCell.identifier)
+    }
+    
+    private func registerDelegate(){
         feedTableView.delegate = self
         feedTableView.dataSource = self
     }
-
 }
 
-// MARK: - Delegate
+// MARK: - UITableViewDelegate
 extension HomeViewController: UITableViewDelegate{
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 488
+        return indexPath.section == 0 ? 84 : 488
     }
 }
 
+// MARK: - UITableViewDataSource
 extension HomeViewController: UITableViewDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return FeedDataModel.sampleData.count
+        if section == 0 {
+            return 1
+        }else {
+            return FeedDataModel.sampleData.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: FeedTableViewCell.identifier, for: indexPath) as? FeedTableViewCell else { return UITableViewCell() }
-        
-        cell.setData(FeedDataModel.sampleData[indexPath.row])
-        return cell
-    }
-}
+        switch indexPath.section{
+        case 0:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: StoryTableViewCell.identifier, for: indexPath) as? StoryTableViewCell else { return UITableViewCell() }
 
-extension HomeViewController: UICollectionViewDataSource{
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return StoryUserDataModel.sampleData.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = storyCollectionView.dequeueReusableCell(withReuseIdentifier: StoryCollectionViewCell.identifier, for: indexPath) as? StoryCollectionViewCell else { return UICollectionViewCell() }
-        cell.setData(userData: StoryUserDataModel.sampleData[indexPath.row])
-        return cell
-    }
-}
+            return cell
+        case 1:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: FeedTableViewCell.identifier, for: indexPath) as? FeedTableViewCell else { return UITableViewCell() }
 
-extension HomeViewController: UICollectionViewDelegateFlowLayout{
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 5
+            cell.setData(FeedDataModel.sampleData[indexPath.row])
+            return cell
+        default:
+            return UITableViewCell()
+        }
     }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 4
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let width = UIScreen.main.bounds.width
-        let cellWidth = width * (58/375)
-        let cellHeight = cellWidth * (72/58)
-        
-        return CGSize(width: cellWidth, height: cellHeight)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top:4, left:4, bottom: 8, right: 4)
-    }
-    
 }
